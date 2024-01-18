9e3eb075d3a0618fe4dab3645755df8abf97e844
helen johnson <helen.linguist@gmail.com>
2024-01-18 16:03:17 -0700

    ch2.3 add my first test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,27 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+/**
+ * 1. open the page
+ * 2. click at get started
+ * 3. mouse hover the language dropdown
+ * 4. click at java
+ * 5. check the url
+ * 6. check the text installing playwright is not bieng displayed
+ * 7. check the text below si displayed
+ * "Playwright is distributed as a set of Maven modules."
+ */
+test.only('check java page', async ({ page }) => {
+  await page.goto('https://playwright.dev');
+  await page.getByRole('link', {name: 'Get Started'}).click();
+  await page.getByRole('button', {name:'Node.js'}).hover();
+  await page.getByText('Java', {exact: true}).click();
+
+  await expect(page).toHaveURL(/java/);
+  await expect(page.getByText('Installing Playwright', {exact: true})).not.toBeVisible();
+   
+  const javaDescription = `Playwright is distributed as a set of Maven modules.`;
+  await expect(page.getByText(javaDescription)).toBeVisible(); 
+
+})
